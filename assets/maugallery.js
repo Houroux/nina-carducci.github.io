30ca126aa9a93a46a073b41389148fc65d6786d3
Houroux <arthurtrovato@gmail.com>
2023-04-19 11:46:39 +0200

    Fix : Navigation dans la modale de la galerie fonctionne

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,8 +154,7 @@
         }
       });
       next =
-        imagesCollection[index] ||
-        imagesCollection[imagesCollection.length - 1];
+        imagesCollection[index-1] || imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     nextImage() {
@@ -192,7 +191,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index+1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
